a295ea6d7a17550586fc1d9f39d4c62600544211
Adam W <Weinzettladam@gmai.com>
2019-04-07 17:26:58 +0200

    Added Type token

--- app/src/main/java/com/doubleyu/myairlines/asynctask/RequestAirlinesData.kt
+++ app/src/main/java/com/doubleyu/myairlines/asynctask/RequestAirlinesData.kt
@@ -4,24 +4,27 @@ import android.os.AsyncTask
 import com.doubleyu.myairlines.Airline
 import com.doubleyu.myairlines.listener.NetworkTaskListener
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import java.net.URL
 
-class RequestAirlinesData(private val listener: NetworkTaskListener<List<Airline>>) : AsyncTask<Void, Void, Array<Airline>>() {
+class RequestAirlinesData(private val listener: NetworkTaskListener<List<Airline>>) : AsyncTask<Void, Void, List<Airline>>() {
 
 
-	override fun onPostExecute(result: Array<Airline>) {
+	override fun onPostExecute(result: List<Airline>) {
 		super.onPostExecute(result)
-		if (result.isEmpty()) listener.onFailure() else listener.onSuccess(result.toList())
+		if (result.isEmpty()) listener.onFailure() else listener.onSuccess(result)
 	}
 
-	override fun doInBackground(vararg params: Void?): Array<Airline> {
+	override fun doInBackground(vararg params: Void?): List<Airline> {
 		val result = URL(apiEndpoint).readText()
-		val airlines: Array<Airline> = Gson().fromJson(result, Array<Airline>::class.java)
+		val airlines: List<Airline> = Gson().fromJson(result, typeToken)
 		return airlines
 	}
 
 	companion object {
 		private const val apiEndpoint: String = "https://www.kayak.com/h/mobileapis/directory/airlines"
+		private val typeToken = object : TypeToken<List<Airline>>() {}.type
+
 	}
 
 }
